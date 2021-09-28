5b0d2953a2797c66306fefc0808c924cffebeff6
AcoGlezRojo <acoidangonzalezrojo@gmail.com>
2021-09-28 18:00:43 +0100

    app finalizada

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Acoidán González on 27/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
